a63d1560854bd55284df2478709210ff49e6eb2c
Mohamed Mukhtar <mukxdev@gmail.com>
2022-08-22 16:05:01 +0300

    contact form fields validation

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,7 +8,6 @@ export default function ContactForm() {
     formState: { errors },
   } = useForm();
   const onSubmit = async (data: any) => {
-
     try {
       // make axios post request
       const response = await axios({
@@ -21,66 +20,81 @@ export default function ContactForm() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   console.log(errors);
 
   return (
-    <form
-      className="container grid max-w-3xl grid-cols-1 gap-6 mx-auto lg:grid-cols-2 "
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col items-start justify-start w-full">
-        <label className="text-base uppercase text-white/30">
-          First and last name*
-        </label>
-        <input
-          className="asambaInput"
-          type="text"
-          {...register("first-last-name", {
-            required: true,
-            maxLength: 80,
-          })}
-        />
-      </div>
-      <div className="flex flex-col items-start justify-start w-full">
-        <label className="text-base uppercase text-white/30">
-          Email address*
-        </label>
-        <input
-          className="asambaInput"
-          type="email"
-          {...register("your-email", { required: true, pattern: /^\S+@\S+$/i })}
-        />
-      </div>
-      <div className="flex flex-col items-start justify-start w-full">
-        <label className="text-base uppercase text-white/30">
-          Company Name*
-        </label>
-        <input
-          className="asambaInput"
-          type="text"
-          {...register("company-name", { required: true, maxLength: 100 })}
-        />
-      </div>
+    <div>
+      <form
+        className="container grid max-w-3xl grid-cols-1 gap-6 mx-auto lg:grid-cols-2 "
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className={`flex flex-col items-start justify-start w-full `}>
+          <label className="text-base uppercase text-white/30">
+            First and last name*
+          </label>
+          <input
+            className={`asambaInput ${
+              errors["first-last-name"] && "!border-red-400"
+            }`}
+            type="text"
+            {...register("first-last-name", {
+              required: true,
+              maxLength: 80,
+            })}
+          />
+        </div>
+        <div className="flex flex-col items-start justify-start w-full">
+          <label className="text-base uppercase text-white/30">
+            Email address*
+          </label>
+          <input
+            className={`asambaInput ${
+              errors["your-email"] && "!border-red-400"
+            }`}
+            type="email"
+            {...register("your-email", {
+              required: true,
+              pattern: /^\S+@\S+$/i,
+            })}
+          />
+        </div>
+        <div className="flex flex-col items-start justify-start w-full">
+          <label className="text-base uppercase text-white/30">
+            Company Name*
+          </label>
+          <input
+            className={`asambaInput ${
+              errors["company-name"] && "!border-red-400"
+            }`}
+            type="text"
+            {...register("company-name", { required: true, maxLength: 100 })}
+          />
+        </div>
 
-      <div className="flex flex-col items-start justify-start w-full">
-        <label className="text-base uppercase text-white/30">
-          Request For*
-        </label>
-        <select
-          className="cursor-pointer asambaInput"
-          {...register("requestFor", { required: true })}
-        >
-          <option defaultValue="Business Deck">Business Deck</option>
-          <option defaultValue="Investment Memorandum">Investment Memorandum</option>
-          <option defaultValue="All Documents">All Documents</option>
-        </select>
-      </div>
-      <input
-        className="px-8 py-3  md:col-span-2 m-0 mx-auto md:mr-0 text-white uppercase duration-150 border border-white rounded-full cursor-pointer bg-white/0 hover:bg-white/10 mt-11 w-fit"
-        type="submit"
-        value="Request Access"
-      />
-    </form>
+        <div className="flex flex-col items-start justify-start w-full">
+          <label className="text-base uppercase text-white/30">
+            Request For*
+          </label>
+          <select
+            className={`cursor-pointer asambaInput ${
+              errors["requestFor"] && "!border-red-400"
+            }`}
+            {...register("requestFor", { required: true })}
+          >
+            <option defaultValue="Business Deck">Business Deck</option>
+            <option defaultValue="Investment Memorandum">
+              Investment Memorandum
+            </option>
+            <option defaultValue="All Documents">All Documents</option>
+          </select>
+        </div>
+        <input
+          className="px-8 py-3  md:col-span-2 m-0 mx-auto md:mr-0 text-white uppercase duration-150 border border-white rounded-full cursor-pointer bg-white/0 hover:bg-white/10 mt-11 w-fit"
+          type="submit"
+          value="Request Access"
+        />
+      </form>
+    </div>
   );
 }
